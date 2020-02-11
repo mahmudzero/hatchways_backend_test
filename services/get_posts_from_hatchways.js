126dad486287ba8175f78eff63a954223c8af253
Mahmud Ahmad <ahmad.mahmud1997@gmail.com>
2020-02-10 21:19:09 -0500

    caching requests to hatchways server

--- services/get_posts_from_hatchways.js
+++ services/get_posts_from_hatchways.js
@@ -2,25 +2,33 @@ var fetch = require('node-fetch');
 
 const HATCHWAYS_API = 'https://hatchways.io/api/assessment/blog/posts';
 
+const calledTags = {};
+
 module.exports = function(tags) {
   var final = {};
 
   var resultPromises = tags.map((tag, index) => {
-    return (
-      fetch(HATCHWAYS_API + '?tag=' + tag)
-        .then((res) => {
-          var _resJSON = res.json();
-          if (!res.ok) {
-            _resJSON.then((_err) => {
-              throw(_err);
-            })
-          } else {
-            return _resJSON;
-          }
-        }).then((resJSON) => {
-          return resJSON;
-        })
-    );
+    if (!calledTags[tag]) {
+      return (
+        fetch(HATCHWAYS_API + '?tag=' + tag)
+          .then((res) => {
+            var _resJSON = res.json();
+            if (!res.ok) {
+              _resJSON.then((_err) => {
+                throw(_err);
+              })
+            } else {
+              return _resJSON;
+            }
+          }).then((resJSON) => {
+            calledTags[tag] = resJSON;
+            return resJSON;
+          })
+      );
+    } else {
+      console.log('using cache for tag: ' + tag);
+      return Promise.resolve(calledTags[tag]);
+    }
   });
 
   return (
